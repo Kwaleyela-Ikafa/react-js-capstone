71e54e40579f090b70e4d5117c9731295e9d4aa6
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-10-04 17:32:11 +0200

    Display Coins

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './Components/Homepage';
+import CoinDetails from './Components/CoinDetails';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/coin/:coinId" element={<CoinDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
